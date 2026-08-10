3563323ef9f6390b0d349ad3cee14cb3239bf2cc
Kristofer <kristhajeb@gmail.com>
2026-08-10 18:22:30 +0800

    make a check for the env only if we need to use it

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -11,11 +11,6 @@ declare global {
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI)
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local",
-  );
-
 const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 
 if (!global.mongoose) global.mongoose = cached;
